b4491181178ab2427fcbaaaca86498cf759d81ae
Adeshina-Qudus <qudusa55@gmail.com>
2025-02-04 00:01:03 +0100

    adding KeyCloakConfig but for this push mainly git ignore

---
+++ src/main/java/dash/meal/mealdash/domain/model/User.java
@@ -0,0 +1,29 @@
+package dash.meal.mealdash.domain.model;
+
+public class User {
+    private String id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private boolean emailVerified;
+    private boolean enabled;
+    private String createdAt;
+    private String image;
+    private String gender;
+    private String dateOfBirth;
+    private String stateOfOrigin;
+    private String maritalStatus;
+    private String stateOfResidence;
+    private String nationality;
+    private String residentialAddress;
+    private UserRole role;
+    private String createdBy;
+    private String alternateEmail;
+    private String alternatePhoneNumber;
+    private String alternateContactAddress;
+    private String password;
+    private String accessToken;
+    private String refreshToken;
+    private String newPassword;
+}
---
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/output/IdentificationManagement.java
@@ -0,0 +1,6 @@
+package dash.meal.mealdash.infrastructure.adapter.output;
+
+import dash.meal.mealdash.application.output.IdentificationOutputPort;
+
+public class IdentificationManagement implements IdentificationOutputPort {
+}
