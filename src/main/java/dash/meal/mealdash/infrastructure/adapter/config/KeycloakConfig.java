9dce2e72fba227a194683f49abb93ccd50676555
Adeshina-Qudus <qudusa55@gmail.com>
2025-02-18 21:08:34 +0100

    worked on key cloak

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/config/KeycloakConfig.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/config/KeycloakConfig.java
@@ -1,12 +1,13 @@
-package dash.meal.mealdash.infrastructure.adapter.config;
+package africa.nkwadoma.nkwadoma.infrastructure.adapters.config;
 
+import org.keycloak.admin.client.Keycloak;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class KeyCloakConfig {
+public class KeycloakConfig {
     @Value("${keycloak.server.url}")
     private String KEYCLOAK_SERVER_URL;
 
@@ -16,14 +17,15 @@ public class KeyCloakConfig {
     @Value("${keycloak.auth.password}")
     private String KEYCLOAK_PASSWORD;
 
-//    @Bean
-//    @Primary
-//    public Keycloak keycloakConfigResolver() {
-//        return Keycloak.getInstance(
-//                KEYCLOAK_SERVER_URL,
-//                "master",
-//                KEYCLOAK_USERNAME,
-//                KEYCLOAK_PASSWORD,
-//                "admin-cli");
-//    }
+    @Bean
+    @Primary
+    public Keycloak keycloakConfigResolver() {
+        return Keycloak.getInstance(
+                KEYCLOAK_SERVER_URL,
+                "master",
+                KEYCLOAK_USERNAME,
+                KEYCLOAK_PASSWORD,
+                "admin-cli");
+    }
+
 }
