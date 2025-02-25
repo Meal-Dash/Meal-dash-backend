e2f7f1ab5cb94c8410b5ab70e0c149c2978af12f
Joseph-Joy-Ender <joy828545@gmail.com>
2025-02-25 16:13:32 +0100

    crud methods

---
+++ src/main/java/dash/meal/mealdash/domain/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package dash.meal.mealdash.domain.exception;
+
+public class ErrorMessage {
+    public static final String USER_CANNOT_BE_NULL = "User cannot be null";
+    public static final String FIRST_NAME_MUST_BE_PROVIDED = "First name is required";
+    public static final String LAST_NAME_MUST_BE_PROVIDED = "Last name is required";
+    public static final String INVALID_NAME_FORMAT = "Invalid name format";
+    public static final String EMAIL_IS_REQUIRED = "Email is required";
+    public static final String INVALID_MAIL_FORMAT = "Invalid email format";
+    public static final String PASSWORD_IS_REQUIRED = "Password is required";
+    public static final String PASSWORD_IS_INVALID = "Password is not valid";
+    public static final String PHONE_NUMBER_IS_REQUIRED = "Phone number is required";
+    public static final String PHONE_NUMBER_IS_INVALID = "Phone number is invalid";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String USER_ALREADY_EXIST = "User already exist";
+    public static final String NO_USERS_FOUND = "No users found";
+}
