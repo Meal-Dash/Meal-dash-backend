dcde00770d5bb62787622460e34178ce7488b091
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-03 17:05:04 +0100

    creating the user service file and implementing the user use case file

---
+++ src/main/java/dash/meal/mealdash/domain/service/notification/user/UserService.java
@@ -0,0 +1,14 @@
+package dash.meal.mealdash.domain.service.notification.user;
+
+import dash.meal.mealdash.application.input.user.UserUseCase;
+import dash.meal.mealdash.infrastructure.adapter.input.rest.data.request.SignupRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserUseCase {
+
+    @Override
+    public String signUp(SignupRequest signupRequest) {
+        return "";
+    }
+}
