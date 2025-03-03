90a52cfd0ab5f611a7f98343aa2418fe595b0dc0
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-03 17:01:19 +0100

    refactoring the file from User to MealDashUser

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/mapper/MealDashMapper.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/mapper/MealDashMapper.java
@@ -1,12 +1,12 @@
 package dash.meal.mealdash.infrastructure.adapter.mapper;
 
-import dash.meal.mealdash.domain.model.User;
+import dash.meal.mealdash.domain.model.MealDashUser;
 import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.UserEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MealDashMapper {
-    UserEntity toUserEntity(User user);
+    UserEntity toUserEntity(MealDashUser user);
 
-    User toUser(UserEntity savedUserEntity);
+    MealDashUser toUser(UserEntity savedUserEntity);
 }
