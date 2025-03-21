40b19f92f9fc9a16ef2c89469ddfb2a009ad0f9e
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-21 17:09:25 +0100

    keycloak signup

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/entity/MealDashEntity.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/entity/MealDashEntity.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserEntity {
+public class MealDashEntity {
     @Id
     private String id;
     private String firstName;
