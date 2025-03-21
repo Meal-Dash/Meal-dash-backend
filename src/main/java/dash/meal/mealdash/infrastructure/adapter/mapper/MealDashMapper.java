470fa7279c777748d29471fa3fa68a6f642e9777
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-21 17:10:23 +0100

    keycloak signup

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/mapper/MealDashMapper.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/mapper/MealDashMapper.java
@@ -1,12 +1,16 @@
 package dash.meal.mealdash.infrastructure.adapter.mapper;
 
 import dash.meal.mealdash.domain.model.MealDashUser;
-import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.UserEntity;
+import dash.meal.mealdash.infrastructure.adapter.input.rest.data.request.SignupRequest;
+import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.MealDashEntity;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MealDashMapper {
-    UserEntity toUserEntity(MealDashUser user);
+    MealDashEntity toUserEntity(MealDashUser user);
 
-    MealDashUser toUser(UserEntity savedUserEntity);
+    MealDashUser toUser(MealDashEntity savedMealDashEntity);
+
+    UserRepresentation toUserRepresentation(SignupRequest request);
 }
