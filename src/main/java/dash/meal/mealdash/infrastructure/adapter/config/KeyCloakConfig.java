76d366c836c108527c49be310e8694a1a40a8e32
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-21 17:08:38 +0100

    keycloak signup

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/config/KeyCloakConfig.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/config/KeyCloakConfig.java
@@ -1,5 +1,8 @@
 package dash.meal.mealdash.infrastructure.adapter.config;
 
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,23 +10,52 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class KeyCloakConfig {
-//    @Value("${keycloak.server.url}")
+//    @Value("${keycloak.server-url}")
 //    private String KEYCLOAK_SERVER_URL;
-//
-//
 //    @Value("${keycloak.auth.user}")
 //    private String KEYCLOAK_USERNAME;
 //    @Value("${keycloak.auth.password}")
 //    private String KEYCLOAK_PASSWORD;
-
+//    @Value("${realm}")
+//    private String REALM;
+//    @Value("${keycloak.base.client}")
+//    private String KEYCLOAK_CLIENT_ID;
+//    @Value("{keycloak.client-secret}")
+//    private String KEYCLOAK_CLIENT_SECRET;
+//
 //    @Bean
 //    @Primary
 //    public Keycloak keycloakConfigResolver() {
 //        return Keycloak.getInstance(
 //                KEYCLOAK_SERVER_URL,
-//                "master",
+//                REALM,
 //                KEYCLOAK_USERNAME,
 //                KEYCLOAK_PASSWORD,
-//                "admin-cli");
+//                KEYCLOAK_CLIENT_SECRET,
+//                KEYCLOAK_CLIENT_ID);
 //    }
+
+    @Value("${keycloak.server-url}")
+    private String authServerUrl;
+
+    @Value("${realm}")
+    private String realm;
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+
+    @Bean
+    public Keycloak keycloakAdminClient() {
+        return KeycloakBuilder.builder()
+                .serverUrl(authServerUrl)
+                .realm(realm)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .scope("email profile")
+                .build();
+    }
 }
