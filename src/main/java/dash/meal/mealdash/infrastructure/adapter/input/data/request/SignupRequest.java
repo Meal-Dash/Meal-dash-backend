6a163601845d70ef2c5a249be4e5594f98ec0321
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-25 09:51:01 +0100

    keycloak signup

---
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/input/data/request/SignupRequest.java
@@ -0,0 +1,33 @@
+package dash.meal.mealdash.infrastructure.adapter.input.data.request;
+
+import dash.meal.mealdash.domain.exception.ErrorMessage;
+import dash.meal.mealdash.domain.exception.RegexPattern;
+import dash.meal.mealdash.domain.model.Location;
+import dash.meal.mealdash.domain.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    @NotBlank
+    @Pattern(regexp = RegexPattern.NAME_REGEX, message = ErrorMessage.INVALID_NAME_FORMAT)
+    private String firstName;
+    @NotBlank
+    @Pattern(regexp = RegexPattern.NAME_REGEX, message = ErrorMessage.INVALID_NAME_FORMAT)
+    private String lastName;
+    @NotBlank
+    @Email(regexp = RegexPattern.EMAIL_REGEX, message = ErrorMessage.INVALID_MAIL_FORMAT)
+    private String email;
+    @NotBlank
+    @Pattern(regexp = RegexPattern.PASSWORD_REGEX, message = ErrorMessage.PASSWORD_IS_INVALID)
+    private String password;
+    private String phoneNumber;
+    private Location deliveryAddress;
+    private UserRole role;
+}
