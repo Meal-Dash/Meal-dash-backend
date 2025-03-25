4ce7c8a5c4077c1486e6d4664a62c5016131c887
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-25 11:15:30 +0100

    keycloak signup

---
+++ src/main/java/dash/meal/mealdash/domain/exception/OtpAdapterException.java
@@ -0,0 +1,7 @@
+package dash.meal.mealdash.domain.exception;
+
+public class OtpAdapterException extends Exception {
+    public OtpAdapterException(String message) {
+        super(message);
+    }
+}
