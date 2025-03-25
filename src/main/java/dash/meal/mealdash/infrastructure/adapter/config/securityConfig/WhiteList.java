e7d3a40e7f1b7f9005006cfc43c08497741b7ffe
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-25 09:52:38 +0100

    keycloak signup

---
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/config/securityConfig/WhiteList.java
@@ -0,0 +1,7 @@
+package dash.meal.mealdash.infrastructure.adapter.config.securityConfig;
+
+public class WhiteList {
+    public static final String[] patterns = {
+            "/api/v1/user/signup"
+    };
+}
