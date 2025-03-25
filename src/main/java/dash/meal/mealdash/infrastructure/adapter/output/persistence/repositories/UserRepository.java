3a57b50664dbd3b5ddd15480478f4dea40a7b405
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-25 09:56:14 +0100

    keycloak signup

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/repositories/UserRepository.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package dash.meal.mealdash.infrastructure.adapter.output.persistence.repositories;
 
-import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.UserEntity;
+import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.MealDashEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-    Optional<UserEntity> findByEmail(String email);
+public interface UserRepository extends JpaRepository<MealDashEntity, String> {
+    Optional<MealDashEntity> findByEmail(String email);
 
 
 }
