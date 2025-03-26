b5b15d1f61934536e21c52d243399695ffb7e476
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-26 11:10:14 +0100

    user signup

--- src/main/java/dash/meal/mealdash/application/input/user/UserUseCase.java
+++ src/main/java/dash/meal/mealdash/application/input/user/UserUseCase.java
@@ -1,7 +1,10 @@
 package dash.meal.mealdash.application.input.user;
 
+import dash.meal.mealdash.domain.exception.MealDashException;
+import dash.meal.mealdash.domain.exception.MealDashUserAdapterException;
+import dash.meal.mealdash.domain.exception.OtpAdapterException;
 import dash.meal.mealdash.infrastructure.adapter.input.data.request.SignupRequest;
 
 public interface UserUseCase {
-    String signUp(SignupRequest signupRequest);
+    String signUp(SignupRequest signupRequest) throws MealDashException, MealDashUserAdapterException, OtpAdapterException;
 }
