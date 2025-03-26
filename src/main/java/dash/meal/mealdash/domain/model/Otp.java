6bdfe05c8780b1470c33fde6dd2f5011336fdc7e
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-26 18:47:23 +0100

    user signup

--- src/main/java/dash/meal/mealdash/domain/model/Otp.java
+++ src/main/java/dash/meal/mealdash/domain/model/Otp.java
@@ -1,8 +1,10 @@
 package dash.meal.mealdash.domain.model;
 
-import dash.meal.mealdash.domain.exception.ErrorMessage;
+import dash.meal.mealdash.domain.message.ErrorMessage;
 import dash.meal.mealdash.domain.exception.OtpAdapterException;
+import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +14,8 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Otp {
+    @Id
+    @UuidGenerator
     private String id;
 
     private String token;
@@ -21,6 +25,12 @@ public class Otp {
     private String emailToken;
 
     private LocalDateTime createdAt;
+
+    public Otp(String token, String email, LocalDateTime createdAt) {
+        this.token = token;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
 
     public void validations(Otp otp) throws OtpAdapterException {
         validateToken(otp.token);
