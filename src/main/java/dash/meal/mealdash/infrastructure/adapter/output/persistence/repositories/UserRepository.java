c7f8f176aedae75a84bf59ab4f30f618853a7a38
Joseph-Joy-Ender <joy828545@gmail.com>
2025-03-26 18:26:00 +0100

    user signup

--- src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/repositories/UserRepository.java
+++ src/main/java/dash/meal/mealdash/infrastructure/adapter/output/persistence/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package dash.meal.mealdash.infrastructure.adapter.output.persistence.repositorie
 
 import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.MealDashEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<MealDashEntity, String> {
     Optional<MealDashEntity> findByEmail(String email);
     boolean existsByEmail(String email);
