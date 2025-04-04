949677ad9b6f3c5fe57b7bd6f00a101d46ea1356
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-04 11:18:23 +0100

    adding global exception handler

--- src/main/java/dash/meal/mealdash/application/output/UserIdentityOutputPort.java
+++ src/main/java/dash/meal/mealdash/application/output/UserIdentityOutputPort.java
@@ -1,12 +1,12 @@
 package dash.meal.mealdash.application.output;
 
-import dash.meal.mealdash.domain.exception.MealDashUserAdapterException;
+import dash.meal.mealdash.domain.exception.UserAdapterException;
 
 import dash.meal.mealdash.domain.model.MealDashUser;
 import org.keycloak.representations.idm.UserRepresentation;
 
 public interface UserIdentityOutputPort {
-    UserRepresentation saveUser(MealDashUser user) throws MealDashUserAdapterException;
-    UserRepresentation getUserByEmail(String email) throws MealDashUserAdapterException;
+    UserRepresentation saveUser(MealDashUser user) throws UserAdapterException;
+    UserRepresentation getUserByEmail(String email) throws UserAdapterException;
     void deleteUser(String email);
 }
