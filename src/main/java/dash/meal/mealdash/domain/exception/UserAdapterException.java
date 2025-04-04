6e2d22480fd5ef55433437ac4f64684ccfacbd94
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-04 11:20:25 +0100

    adding global exception handler

---
+++ src/main/java/dash/meal/mealdash/domain/exception/UserAdapterException.java
@@ -0,0 +1,8 @@
+package dash.meal.mealdash.domain.exception;
+
+public class UserAdapterException extends MealDashException{
+
+    public UserAdapterException(String message){
+        super(message);
+    }
+}
