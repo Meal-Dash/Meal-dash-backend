25cf168d2a33b88b632cd43ae11b236dd1d5b967
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-04 11:13:32 +0100

    adding global exception handler

--- src/main/java/dash/meal/mealdash/application/input/user/UserUseCase.java
+++ src/main/java/dash/meal/mealdash/application/input/user/UserUseCase.java
@@ -1,11 +1,10 @@
 package dash.meal.mealdash.application.input.user;
 
 import dash.meal.mealdash.domain.exception.MealDashException;
-import dash.meal.mealdash.domain.exception.MealDashUserAdapterException;
+import dash.meal.mealdash.domain.exception.UserAdapterException;
 import dash.meal.mealdash.domain.exception.OtpAdapterException;
 import dash.meal.mealdash.domain.model.MealDashUser;
-import dash.meal.mealdash.infrastructure.adapter.input.data.request.CustomerSignupRequest;
 
 public interface UserUseCase {
-    String signUp(MealDashUser user) throws MealDashException, MealDashUserAdapterException, OtpAdapterException;
+    String signUp(MealDashUser user) throws MealDashException, UserAdapterException, OtpAdapterException;
 }
