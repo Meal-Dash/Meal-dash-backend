ce0e870f89b4aac834a720973863b29746201d52
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-04 07:18:48 +0100

    refactoring

--- src/main/java/dash/meal/mealdash/domain/service/notification/user/UserService.java
+++ src/main/java/dash/meal/mealdash/domain/service/notification/user/UserService.java
@@ -3,7 +3,7 @@ package dash.meal.mealdash.domain.service.notification.user;
 import dash.meal.mealdash.application.input.email.EmailUseCase;
 import dash.meal.mealdash.application.input.otp.OtpUseCase;
 import dash.meal.mealdash.application.input.user.UserUseCase;
-import dash.meal.mealdash.application.output.MealDashUserIdentityOutputPort;
+import dash.meal.mealdash.application.output.UserIdentityOutputPort;
 import dash.meal.mealdash.application.output.UserOutputPort;
 import dash.meal.mealdash.domain.message.ErrorMessage;
 import dash.meal.mealdash.domain.exception.MealDashException;
@@ -12,9 +12,7 @@ import dash.meal.mealdash.domain.exception.OtpAdapterException;
 import dash.meal.mealdash.domain.message.SuccessMessage;
 import dash.meal.mealdash.domain.model.MealDashUser;
 import dash.meal.mealdash.domain.model.Otp;
-import dash.meal.mealdash.infrastructure.adapter.input.data.request.CustomerSignupRequest;
 import dash.meal.mealdash.infrastructure.adapter.output.mapper.MealDashMapper;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -25,14 +23,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService implements UserUseCase {
     private final UserOutputPort userOutputPort;
-    private final MealDashUserIdentityOutputPort mealDashUserIdentityOutputPort;
+    private final UserIdentityOutputPort userIdentityOutputPort;
     private final MealDashMapper mealDashMapper;
     private final OtpUseCase otpUseCase;
     private final EmailUseCase emailUseCase;
 
 
     @Override
-    @Transactional
     public String signUp(MealDashUser user) throws MealDashException, MealDashUserAdapterException, OtpAdapterException {
         if (user == null) throw new MealDashException(ErrorMessage.INVALID_USER_DETAILS);
         user.validate();
@@ -42,7 +39,7 @@ public class UserService implements UserUseCase {
         log.info("OTP {}",otp);
         log.info("OTP: {} generated for user: {}", otp.getToken(), user.getEmail());
 
-        UserRepresentation kcMealUser = mealDashUserIdentityOutputPort.saveUser(user);
+        UserRepresentation kcMealUser = userIdentityOutputPort.saveUser(user);
         log.info("keycloak signed up user {}", kcMealUser);
         user.setId(kcMealUser.getId());
 
