619025ba756eea94a3c88e5c3a288d023f208682
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-20 00:56:23 +0100

    keycloak-refactor

--- src/main/java/dash/meal/mealdash/domain/validation/MealDashValidator.java
+++ src/main/java/dash/meal/mealdash/domain/validation/MealDashValidator.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 
 @Slf4j
-public class MealDashValidation {
+public class MealDashValidator {
     public static void validateObjectInstance(Object instance, String message) throws MealDashException {
         if (ObjectUtils.isEmpty(instance)){
             log.error("Object instance validation failed {}", message);
