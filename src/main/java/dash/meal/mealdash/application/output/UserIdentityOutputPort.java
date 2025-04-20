e12bebbc63c5e9e9d6f0089d365c19ffc44435c8
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-20 01:16:08 +0100

    keycloak-refactor

--- src/main/java/dash/meal/mealdash/application/output/UserIdentityOutputPort.java
+++ src/main/java/dash/meal/mealdash/application/output/UserIdentityOutputPort.java
@@ -1,13 +1,21 @@
 package dash.meal.mealdash.application.output;
 
+import dash.meal.mealdash.domain.exception.MealDashException;
 import dash.meal.mealdash.domain.exception.UserAdapterException;
 
 import dash.meal.mealdash.domain.model.MealDashUser;
+import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.idm.UserRepresentation;
 
+import java.util.Optional;
+
 public interface UserIdentityOutputPort {
-    UserRepresentation saveUser(MealDashUser user) throws UserAdapterException;
-    UserRepresentation getUserByEmail(String email) throws UserAdapterException;
-    void deleteUser(String email);
-    void verifyEmail(String email);
+    MealDashUser createUser(MealDashUser user) throws MealDashException;
+    Optional<MealDashUser> getUserByEmail(String email) throws UserAdapterException;
+    void deleteUser(MealDashUser user) throws MealDashException;
+    MealDashUser enableUserAccount(MealDashUser user) throws MealDashException;
+    MealDashUser createPassword(MealDashUser user) throws MealDashException;
+    void setPassword(MealDashUser user) throws MealDashException;
+    AccessTokenResponse login(MealDashUser user) throws MealDashException;
+    UserRepresentation getUserRepresentation(MealDashUser user, boolean exactMatch) throws MealDashException;
 }
