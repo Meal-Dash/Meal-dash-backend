235472ec12b5580969aaceee720f5aaeaf0fa75e
Joseph-Joy-Ender <joy828545@gmail.com>
2025-04-22 15:30:52 +0300

    keycloak-refactor

---
+++ src/main/java/dash/meal/mealdash/domain/service/notification/email/DevEmailService.java
@@ -0,0 +1,31 @@
+package dash.meal.mealdash.domain.service.notification.email;
+
+import dash.meal.mealdash.application.input.email.EmailUseCase;
+import dash.meal.mealdash.domain.model.Otp;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+import java.time.LocalDate;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class DevEmailService implements EmailUseCase {
+
+    @Override
+    public void sendOtp(Otp otp, String username) {
+        Context context = new Context();
+        context.setVariable("user_name", username);
+        context.setVariable("user_code", otp.getToken());
+        context.setVariable("user_email", otp.getEmail());
+        context.setVariable("year", LocalDate.now().getYear());
+
+    }
+
+    @Override
+    public String verifyEmail(String email) {
+        return "";
+    }
+}
