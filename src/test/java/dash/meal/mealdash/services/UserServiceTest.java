9f5c911dd194d94af6eaba4bfd94a6487c4ae575
Joseph-Joy-Ender <joy828545@gmail.com>
2025-05-08 10:20:34 +0100

    email-verification use case

--- src/test/java/dash/meal/mealdash/services/UserServiceTest.java
+++ src/test/java/dash/meal/mealdash/services/UserServiceTest.java
@@ -7,6 +7,7 @@ import dash.meal.mealdash.application.output.user.UserIdentityOutputPort;
 import dash.meal.mealdash.application.output.user.UserOutputPort;
 import dash.meal.mealdash.domain.exception.MealDashException;
 import dash.meal.mealdash.domain.exception.OtpAdapterException;
+import dash.meal.mealdash.domain.exception.UserAdapterException;
 import dash.meal.mealdash.domain.message.ErrorMessage;
 import dash.meal.mealdash.domain.message.SuccessMessage;
 import dash.meal.mealdash.domain.model.MealDashUser;
@@ -21,9 +22,9 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -47,21 +48,24 @@ public class UserServiceTest {
 
     @Mock
     private EmailUseCase emailUseCase;
+
     private MealDashUser user;
+
+    private Otp otp;
+
     @BeforeEach
     void setUp(){
         user = TestData.buildTestUser("johndoe@gmail.com");
-    }
 
-    @Test
-    void signup() throws OtpAdapterException, MealDashException {
-
-        Otp otp = Otp.builder()
+        otp = Otp.builder()
                 .token("123543")
                 .email(user.getEmail())
                 .createdAt(LocalDateTime.now())
                 .build();
+    }
 
+    @Test
+    void signup() throws OtpAdapterException, MealDashException {
         when(userOutputPort.existsByEmail(user.getEmail())).thenReturn(false);
         when(userIdentityOutputPort.createUser(user)).thenReturn(user);
         when(userOutputPort.save(user)).thenReturn(user);
@@ -80,6 +84,49 @@ public class UserServiceTest {
 
         MealDashException mealDashException = assertThrows(MealDashException.class, ()-> userService.signUp(user));
         assertEquals(ErrorMessage.USER_ALREADY_EXIST, mealDashException.getMessage());
+    }
+
+    @Test
+    void verifyUserEmail() throws MealDashException {
+        when(otpUseCase.findByEmailAndToken(user.getEmail() + user.getToken())).thenReturn(otp);
+        when(userOutputPort.findByEmail(otp.getEmail())).thenReturn(user);
+        doNothing().when(otpUseCase).deleteOtp(otp);
+        when(userIdentityOutputPort.enableUserAccount(user)).thenReturn(new MealDashUser());
+        when(userOutputPort.save(user)).thenReturn(new MealDashUser());
+
+        String successMessage = userService.verifyEmail(user);
+        assertNotNull(successMessage);
+        assertEquals(SuccessMessage.EMAIL_VERIFIED_SUCCESSFULLY, successMessage);
+    }
+
+    @Test
+    void verifyUserEmailThrowExceptionWhenUserIsNull() throws MealDashException {
+        when(otpUseCase.findByEmailAndToken(user.getEmail() + user.getToken())).thenReturn(otp);
+        when(userOutputPort.findByEmail(otp.getEmail())).thenReturn(null);
+
+       MealDashException exception = assertThrows(MealDashException.class, ()-> userService.verifyEmail(user));
+       assertEquals(ErrorMessage.USER_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenOtpIsExpired() {
+        MealDashUser user = new MealDashUser();
+        user.setEmail("user@example.com");
+        user.setToken("someToken");
+
+        Otp expiredOtp = new Otp();
+        expiredOtp.setEmail(user.getEmail());
+        expiredOtp.setToken(user.getToken());
+        expiredOtp.setCreatedAt(LocalDateTime.now(ZoneOffset.UTC).minusMinutes(6));
+
+        when(otpUseCase.findByEmailAndToken(user.getEmail() + user.getToken()))
+                .thenReturn(expiredOtp);
+
+        MealDashException exception = assertThrows(MealDashException.class, () -> {
+            userService.verifyEmail(user);
+        });
+
+        assertEquals(ErrorMessage.OTP_EXPIRED_OR_INVALID, exception.getMessage());
     }
 
 }
