7999f8533a6340c7edcc727aacaee2dfe4dd2547
Joseph-Joy-Ender <joy828545@gmail.com>
2025-05-08 15:38:20 +0100

    otp service

--- src/main/java/dash/meal/mealdash/domain/service/notification/otp/OtpService.java
+++ src/main/java/dash/meal/mealdash/domain/service/notification/otp/OtpService.java
@@ -3,10 +3,11 @@ package dash.meal.mealdash.domain.service.notification.otp;
 import dash.meal.mealdash.application.input.otp.OtpUseCase;
 import dash.meal.mealdash.application.output.user.OtpOutputPort;
 import dash.meal.mealdash.domain.exception.OtpAdapterException;
+import dash.meal.mealdash.domain.message.ErrorMessage;
 import dash.meal.mealdash.domain.model.Otp;
 import dash.meal.mealdash.infrastructure.adapter.output.mapper.MealDashMapper;
 import dash.meal.mealdash.infrastructure.adapter.output.persistence.entity.OtpEntity;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OtpService implements OtpUseCase {
     private final OtpOutputPort otpOutputPort;
     private final MealDashMapper mealDashMapper;
@@ -23,16 +24,31 @@ public class OtpService implements OtpUseCase {
 
     @Override
     public Otp generateOtp(String email) throws OtpAdapterException {
-        String otpToken = generateOtpToken();
-        log.info("Generated OTP: {}", otpToken);
+        SecureRandom secureRandom = new SecureRandom();
+        int token = secureRandom.nextInt(999999);
 
-        OtpEntity otpEntity = new OtpEntity(otpToken, email, LocalDateTime.now());
+        StringBuilder otp = new StringBuilder(String.valueOf(token));
+        while (otp.length() < 6) {
+            otp.insert(0, "0");
+        }
+
+        OtpEntity otpEntity = new OtpEntity(otp.toString(), email, LocalDateTime.now(), email + otp);
         log.info("Mapped OTP: {}", otpEntity);
 
         otpOutputPort.save(mealDashMapper.toOtp(otpEntity));
         log.info("Saved OTP: {}", otpEntity);
 
-        return new Otp(otpEntity.getToken(), otpEntity.getEmail(),  otpEntity.getCreatedAt());
+        return new Otp(otpEntity.getToken(), otpEntity.getEmail(),  otpEntity.getCreatedAt(), otpEntity.getEmailToken());
+    }
+
+    @Override
+    public Otp findByEmailAndToken(String emailToken) {
+        return otpOutputPort.findByEmailTokenIgnoreCase(emailToken).orElseThrow(() -> new OtpAdapterException(ErrorMessage.INVALID_CODE));
+    }
+
+    @Override
+    public void deleteOtp(Otp otp) {
+        otpOutputPort.delete(otp);
     }
 
     private String generateOtpToken() {
